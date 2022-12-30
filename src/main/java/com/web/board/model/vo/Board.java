be6c1ac508fc5849d125c4440f9bbbb6fe26e687
hyunw <hyunw@LAPTOP-ONJ1H4CI>
2022-12-30 12:00:50 +0900

    05_Hello MVC 업로드

---
+++ src/main/java/com/web/board/model/vo/Board.java
@@ -0,0 +1,25 @@
+package com.web.board.model.vo;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.web.member.model.vo.Member;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Board {
+	private int boardNo;
+	private String boardTitle;
+	private String boardWriter;
+//	private Member boardWriter;
+	private String boardContent;
+	private String boardOriginalFilename;
+	private String boardRenamedFilename;
+	private Date boardDate;
+	private int boardReadcount;
+//	private List<BoardComment> comments;
+	
+}
